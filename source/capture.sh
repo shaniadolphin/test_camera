b37d11db218c8f6815aee4d364fb25cf889a1a76
zxjun <349948204@qq.com>
2019-04-25 18:05:45 +0800

    添加修改后的拍照脚本

--- source/capture.sh
+++ source/capture.sh
@@ -1,9 +1,9 @@
-echo "Capture test!!!!!"
-echo "shell0:$0"
+#echo "Capture test!!!!!"
+#echo "$0"
 echo "width:$1"
 echo "name:$2"
-echo "dev:$3"
-echo $PWD
-rm -rf *.jpg
+#echo "dev:$3"
+#echo $PWD
+#rm -rf *.jpg
 #rm -rf *.yuv
-./capture -d /dev/video2 -d /dev/$3 -i $2 -w $1 -m
+./capture -d /dev/$3 -i $2 -w $1 -m
